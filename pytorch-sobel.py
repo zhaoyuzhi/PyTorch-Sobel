70c5c64212e135ecc8411b6494e335ee075f6fe0
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2020-12-23 09:56:24 +0800

    Create pytorch-sobel.py

---
+++ pytorch-sobel.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class GradLayer(nn.Module):
+
+    def __init__(self):
+        super(GradLayer, self).__init__()
+        kernel_v = [[0, -1, 0],
+                    [0, 0, 0],
+                    [0, 1, 0]]
+        kernel_h = [[0, 0, 0],
+                    [-1, 0, 1],
+                    [0, 0, 0]]
+        kernel_h = torch.FloatTensor(kernel_h).unsqueeze(0).unsqueeze(0)
+        kernel_v = torch.FloatTensor(kernel_v).unsqueeze(0).unsqueeze(0)
+        self.weight_h = nn.Parameter(data=kernel_h, requires_grad=False)
+        self.weight_v = nn.Parameter(data=kernel_v, requires_grad=False)
+
+    def get_gray(self,x):
+        ''' 
+        Convert image to its gray one.
+        '''
+        gray_coeffs = [65.738, 129.057, 25.064]
+        convert = x.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
+        x_gray = x.mul(convert).sum(dim=1)
+        return x_gray.unsqueeze(1)
+
+    def forward(self, x):
+        # x_list = []
+        # for i in range(x.shape[1]):
+        #     x_i = x[:, i]
+        #     x_i_v = F.conv2d(x_i.unsqueeze(1), self.weight_v, padding=1)
+        #     x_i_h = F.conv2d(x_i.unsqueeze(1), self.weight_h, padding=1)
+        #     x_i = torch.sqrt(torch.pow(x_i_v, 2) + torch.pow(x_i_h, 2) + 1e-6)
+        #     x_list.append(x_i)
+
+        # x = torch.cat(x_list, dim=1)
+        if x.shape[1] == 3:
+            x = self.get_gray(x)
+
+        x_v = F.conv2d(x, self.weight_v, padding=1)
+        x_h = F.conv2d(x, self.weight_h, padding=1)
+        x = torch.sqrt(torch.pow(x_v, 2) + torch.pow(x_h, 2) + 1e-6)
+
+        return x
+
+class GradLoss(nn.Module):
+
+    def __init__(self):
+        super(GradLoss, self).__init__()
+        self.loss = nn.L1Loss()
+        self.grad_layer = GradLayer()
+
+    def forward(self, output, gt_img):
+        output_grad = self.grad_layer(output)
+        gt_grad = self.grad_layer(gt_img)
+        return self.loss(output_grad, gt_grad)
+
+if __name__ == "__main__":
+
+    import cv2
+    import numpy as np
+    
+    net = GradLayer()
+    img = cv2.imread('example.JPEG')
+    a = img.shape
+    
+    img = (img / 255.0).astype(np.float32)
+    img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0)
+    img = net(img) # input img: [0, 1], torch.float32
+    img = (img[0, :, :, :].permute(1, 2, 0).numpy() * 255.0).astype(np.uint8)
+    
+    b = img.shape
+    assert a == b
+    cv2.imshow('pytorch sobel', img)
+    cv2.waitKey(0)
